38274aef54e18cbbf3c98623542570bffa407209
adityanarayan26 <adityanarayanraikoti@gmail.com>
2024-12-10 21:04:43 +0530

    updated again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,14 @@
-<<<<<<< HEAD
+
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {  Route, Routes } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
-import { Toaster } from 'react-hot-toast'
 import Dashboard from './pages/Dashboard'
-
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <Toaster position="bottom-right" />
-        <Routes>
-          <Route path='/' element={<Dashboard />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/create-account' element={<SignupPage />} />
-        </Routes>
-      </div>
-    </Router>
-  )
-}
-=======
-import React from 'react';
 import { Navigate, Route, Routes } from 'react-router';
 import { Toaster } from 'react-hot-toast';
-import { UseStore } from './store/UseStore'; // Import your Zustand store
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import Dashboard from './pages/Dashboard';
+import { UseStore } from './store/UseStore'; 
+
+
 
 const App = () => {
   const {isCheckingAuth} = UseStore()
@@ -53,6 +34,6 @@ const App = () => {
     </div>
   );
 };
->>>>>>> a56740805dfd87db323079b5d500787d01171724
+
 
 export default App;
